02306349b354f76c6052936123731eecd598ab6a
andrew.tran <andrew.tran@ericsson.com>
2019-03-18 15:47:10 -0400

    Initial Upload

---
+++ Assets/src/Camera/FollowCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public GameObject cameraTarget;
+    public GameObject player;
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start() {
+        offset = cameraTarget.transform.position - transform.position;
+    }
+
+    void LateUpdate() {
+        setPosition();
+
+        transform.LookAt(cameraTarget.transform);
+    }
+
+    void setPosition() {
+        Vector3 playerPos = player.transform.position;
+        Vector3 cameraPos = transform.position;
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Linecast(playerPos, cameraPos, out hit)) {
+
+        }
+        else {
+            float currentAngle = transform.eulerAngles.y;
+            float desiredAngle = cameraTarget.transform.eulerAngles.y;
+            float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            transform.position = cameraTarget.transform.position - (rotation * offset);
+        }
+    }
+}
---
+++ Assets/src/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Inputs {
+
+}
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private int movementSpeed;
+    private int turnSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        movementSpeed = 50;
+        turnSpeed = 1;
+    }
+
+    void Update()
+    {
+        mapInputs();
+    }
+
+    void mapInputs() {
+        if (Input.GetAxis("Vertical") != 0) {
+            addForceInDirection(movementSpeed, Vector3.back * Input.GetAxis("Vertical"));
+        }
+        if (Input.GetAxis("Horizontal") != 0) {
+            addForceInDirection(movementSpeed, Vector3.right * Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("CameraRotate") != 0) {
+            rotatePlayer(Input.GetAxis("CameraRotate"));
+        }
+
+        noInput();
+    }
+
+    void addForceInDirection(int amount, Vector3 direction) {
+        rb.AddRelativeForce(direction * amount);
+    }
+
+    void rotatePlayer(float amount) {
+        print(amount);
+        this.transform.Rotate(Vector3.up, amount);
+    }
+
+    void noInput() {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
