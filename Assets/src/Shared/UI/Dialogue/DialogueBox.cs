5f8ef88caf66fd76cf818468021d258e0edde483
ATranimal <tran.andrew.a@gmail.com>
2019-04-12 00:33:10 -0400

    Initial Dialogue Implementation

---
+++ Assets/src/Shared/UI/Dialogue/DialogueBox.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using System.IO;
+
+public class DialogueBox : MonoBehaviour
+{
+    public GameObject emojiPrefab;
+
+    TextAsset text;
+    string pathToText = "Dialogue/Test/test";  
+
+    string[] parsedDialogue;
+
+    float counter;
+    int index;
+    int emojiCount;
+    float currentSpeed;
+    
+    void Start()
+    {  
+        parseDialogue();
+        emojiCount = 0;
+
+        try {  
+            currentSpeed = float.Parse(parsedDialogue[0]);
+            index = 1;
+        } catch {
+            Debug.LogError("text file " + pathToText + " does not start with a float");
+        }
+    }
+
+    void Update()
+    {
+        float newSpeed;
+        bool timeBetweenEmojiHasElapsed = counter > currentSpeed;
+
+        if (index < parsedDialogue.Length) {
+            if (float.TryParse(parsedDialogue[index], out newSpeed)) {
+                currentSpeed = newSpeed;
+                index += 1;
+            } else {
+                if (timeBetweenEmojiHasElapsed) {
+                    createEmoji(parsedDialogue[index]);
+                    counter = 0;
+                    index += 1;
+                }
+            }
+        }
+        else {
+            // wait for input to destroy box? or set timeout for box to disappear on its own.
+        }
+        
+        counter += Time.deltaTime;
+    }
+
+    private void parseDialogue() {
+        text = Resources.Load<TextAsset>(pathToText);
+        parsedDialogue = text.text.Split(
+            new string[] { "\r\n", "\n"},
+            StringSplitOptions.None
+        );
+    }
+
+    private void createEmoji(string nameOfEmoji) {
+        Vector3 newPosition = this.transform.position + new Vector3(emojiCount * 50, 0, 0);
+        emojiCount += 1;
+
+        GameObject newEmoji = Instantiate(
+            emojiPrefab,
+            newPosition,
+            Quaternion.Euler(0, 0, 0)
+        );
+        newEmoji.transform.SetParent(this.transform.parent);
+        newEmoji.GetComponent<Emoji>().setImage(nameOfEmoji);
+    }
+}
---
+++ Assets/src/Shared/UI/Dialogue/Emoji.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Emoji : MonoBehaviour
+{
+    Image img;
+
+    float counter;
+    float fadeInTime;
+
+    public void setImage(string nameOfImage) {
+        string[] splitPaths = nameOfImage.Split('_');
+        string pathToimage = "Emoji/" + splitPaths[0];
+        for (int index = 1; index < splitPaths.Length; index++) {
+            pathToimage += "/" + splitPaths[index];
+        }
+
+        print(pathToimage);
+        img.sprite = Resources.Load<Sprite>(pathToimage);
+    }
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+
+        counter = 0;
+        fadeInTime = 0f;    
+    }
+
+    void Update()
+    {
+        // float transparencyValue = counter / fadeInTime;
+        img.color = new Color(
+            img.color.r,
+            img.color.g,
+            img.color.b,
+            1
+        );
+
+        if (counter > fadeInTime) {
+            counter += Time.deltaTime;
+        }
+    }   
+}
